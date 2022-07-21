7471fe70c0fe915b3057607ee466cf4b4b3dd825
Feliks Czart <czart78@gmail.com>
2022-07-21 17:51:40 +0200

    url for device

--- src/main/kotlin/com/felikscoding/routes/RabbitRoute.kt
+++ src/main/kotlin/com/felikscoding/routes/RabbitRoute.kt
@@ -11,11 +11,11 @@ private const val BASE_URL_EMULATOR = "http://10.0.2.2:8100"
 private const val BASE_URL_DEVICE = "http://192.168.1.101:8100"
 
 private val rabbits = listOf(
-    Rabbit("Carl", "A cute brown rabbit", "$BASE_URL_EMULATOR/rabbits/rabbit1.jpg"),
-    Rabbit("Emma", "Emma likes to eat apples", "$BASE_URL_EMULATOR/rabbits/rabbit2.jpg"),
-    Rabbit("Florian", "Florian is always hungry", "$BASE_URL_EMULATOR/rabbits/rabbit3.jpg"),
-    Rabbit("Federico", "Federico likes to climb mountains", "$BASE_URL_EMULATOR/rabbits/rabbit4.jpg"),
-    Rabbit("Gina", "Gina is a true beauty", "$BASE_URL_EMULATOR/rabbits/rabbit5.jpg"),
+    Rabbit("Carl", "A cute brown rabbit", "$BASE_URL_DEVICE/rabbits/rabbit1.jpg"),
+    Rabbit("Emma", "Emma likes to eat apples", "$BASE_URL_DEVICE/rabbits/rabbit2.jpg"),
+    Rabbit("Florian", "Florian is always hungry", "$BASE_URL_DEVICE/rabbits/rabbit3.jpg"),
+    Rabbit("Federico", "Federico likes to climb mountains", "$BASE_URL_DEVICE/rabbits/rabbit4.jpg"),
+    Rabbit("Gina", "Gina is a true beauty", "$BASE_URL_DEVICE/rabbits/rabbit5.jpg"),
 )
 
 fun Route.randomRabbit() {
